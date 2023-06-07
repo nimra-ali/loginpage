07e0f5125a2120f237f797c38a9e3946eb2087db
nimra-ali <nimrakhan098ali@gmail.com>
2023-06-07 03:40:02 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+function loginform(){
+    var username = document.getElementById("input1"
+    ).value;
+    let email = document.getElementById('input2').value;
+    let password = document.getElementById('input3').value;
+if(username === 'nimra' && email === 'abc@gmail.com' && password === 'nimraali'){
+alert('LOGIN SUCCESFULLY');
+}
+else( alert("DETAILS ARE INCORRECT"));
+}
